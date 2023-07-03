b026d815fce805282c0093fa7cd19464c7f14c4f
Pamela Cruz <85598626+Pamec7@users.noreply.github.com>
2023-07-02 22:46:48 -0500

    Delete src directory

--- src/Anfitrion.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Anfitrion {
-    private int idAnfitrion;
-    private String nombre;
-    private String telefono;
-    private Alojamiento alojamiento;
-
-}
